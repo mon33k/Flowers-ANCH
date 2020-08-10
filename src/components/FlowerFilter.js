bedb9ba9411a4e610e87ead28d3ec9eb623bfbf2
mon33k <moniquemojica@ac.c4q.nyc>
2020-08-10 16:36:00 -0400

    added flower data

--- src/components/FlowerFilter.js
+++ src/components/FlowerFilter.js
@@ -1,21 +1,45 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import '../stylesheets/flowerFilter.css';
+import {Dropdown, DropdownButton} from 'react-bootstrap';
+import flowerData from '../data/flowerData.json';
 
 class FlowerFilter extends Component {
     constructor() {
         super()
         this.state = {
-
+            // flowers: []
         }
     }
-    
 
+componentDidMount(){
+    // const {flowers} = this.state
+    flowerData.forEach(element => {
+        console.log("flower json", element)
+    });
+}
 
 
     render() {
-        return(
+        return (
             <div className='main-container'>
-                <div>all flowers</div>
+                <h1>Filter Flowers</h1>
+                <div className="leftNav col-md-4">
+                    <DropdownButton
+                        as='ButtonGroup'
+                        key='right'
+                        id='dropdown-button-drop-right'
+                        drop='right'
+                        variant="secondary"
+                        size='lg'
+                        title='Select Flower Breed'
+                    >
+                        <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                        <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                        <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+                    </DropdownButton>
+                </div>
             </div>
         )
     }
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react'
+import {Jumbotron, Button, Container} from 'react-bootstrap'
 
 const LandingPage = () => {
     return (
         <div>
-            Landing Page
+            <Jumbotron fluid>
+                <Container>
+                    <h1>Animal Crossing New Horizons Flower Index</h1>
+                    <p>
+                        You can choose to filter a specific breed of flower or .. etc
+                    </p>
+                    <p>
+                        <Button variant="primary">Learn more</Button>
+                    </p>
+                </Container>
+            </Jumbotron>
         </div>
     )
 }
